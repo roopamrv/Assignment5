18538e6df736da0586d47319a66f7bfa927e12ad
roopamrv <roopamverma9717@gmail.com>
2023-07-02 20:36:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,26 +3,21 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from whoosh.index import create_in, open_dir
 from whoosh.fields import Schema, TEXT, ID
-from whoosh.qparser import QueryParser
+from whoosh.qparser import MultifieldParser, QueryParser, Operator
+from whoosh.query import Or
 import os
 from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 from bs4 import BeautifulSoup
-from difflib import SequenceMatcher
 
 app = Flask(__name__)
-
 app.secret_key = 'your secret key'
-
 dir_path = os.path.dirname(os.path.realpath(__file__)) + '/index_dir'
 UPLOAD_FOLDER = dir_path
-# ALLOWED_EXTENSIONS = {'txt'}
-
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() 
-# in ALLOWED_EXTENSIONS
 
 def preprocess_data(text):
     # Tokenization
@@ -54,19 +49,19 @@ def create_index(directory_path):
         file_path = os.path.join(directory_path, file_name)
         if os.path.isfile(file_path):
             with open(file_path, 'r', encoding='latin-1') as file:
-
                 lines = file.readlines()
 
             for line_number, line in enumerate(lines):
                 preprocessed_line = preprocess_data(line.strip())
-                # print("PREPROCESSED LINE: ", preprocessed_line)
                 writer.add_document(doc_id=str(file_name), content=' '.join(preprocessed_line))
 
     writer.commit()
 
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -89,31 +84,20 @@ def upload():
     return '<h1>Unsuccessful</h1>'
 
 
-def get_line_number(doc_id, keyword):
+def get_line_numbers(doc_id, keywords):
     file_path = os.path.join(app.config['UPLOAD_FOLDER'], doc_id)
     line_numbers = []
     lines_sentence = []
-    ix = open_dir(app.config['UPLOAD_FOLDER'])
-    searcher = ix.searcher()
-    query_parser = QueryParser("doc_id", schema=ix.schema)
-    parsed_query = query_parser.parse(doc_id)
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
 
-    results = searcher.search(parsed_query, limit=1)
-    
-    if results:
-        stored_doc = results[0]
-        stored_lines = stored_doc['content'].split('\n')
-
-        with open(file_path, 'r', encoding='utf-8') as file:
-            lines = file.readlines()
-
-            for line_number, line in enumerate(lines, start=1):
+        for line_number, line in enumerate(lines, start=1):
+            for keyword in keywords:
                 if keyword.lower() in line.lower():
                     line_numbers.append(line_number)
-                    lines_sentence.append(line)
+                    lines_sentence.append(line.strip())
 
-    return line_numbers,lines_sentence
-
+    return line_numbers, lines_sentence
 
 
 def get_line_text(line):
@@ -121,53 +105,48 @@ def get_line_text(line):
     line_text = soup.get_text(separator=' ')
     return line_text
 
-def process_lines(lines):
-    processed_lines = []
-    for line in lines:
-        line_text = get_line_text(line)
-        processed_lines.append(line_text)
-    return processed_lines
 
-def search_documents(keyword):
+def search_documents(keywords):
     results = []
 
     ix = open_dir(app.config['UPLOAD_FOLDER'])
-    print("IX: ",ix)
     searcher = ix.searcher()
     query_parser = QueryParser("content", schema=ix.schema)
-    parsed_query = query_parser.parse(keyword)
-    print("Parser query: ",parsed_query)
-    search_results = searcher.search(parsed_query)
+    
+    # Create a separate query for each keyword and combine them with OR operator
+    queries = [query_parser.parse(keyword) for keyword in keywords]
+    combined_query = Or(queries)
 
-    print("inside searched documents")
-    print("searched results:", search_results)
+    search_results = searcher.search(combined_query)
 
-    preprocessed_line = []
     for result in search_results:
-        print("Result: ",result)
         doc_id = result['doc_id']
         score = result.score
-        print("DOC ID: ",doc_id)
-        line_number, line= get_line_number(doc_id, keyword)
-        
+        line_numbers, lines_sentence = get_line_numbers(doc_id, keywords)
+        highlight = result.highlights("content", top=1)
+        line_text = get_line_text(highlight)
+
         result = {
             'doc_id': doc_id,
             'score': score,
-            'line_number': line_number,
-            'line': line
+            'line_numbers': line_numbers,
+            'lines_sentence': lines_sentence,
+            'highlight': line_text
         }
         results.append(result)
+
     return results
 
 
 
 @app.route('/search', methods=['POST'])
 def search():
-    keyword = str(request.form.get('key'))
-    print("KEYWORD: ",keyword)
-    results = search_documents(keyword)
-    print("RESULTS:  ",results)
+    keywords = request.form.get('key').split()
+    print(keywords)
+    results = search_documents(keywords)
+    print(results)
     return render_template("result.html", results=results)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8587, debug=True)
